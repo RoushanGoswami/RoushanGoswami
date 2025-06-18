f11948e9532d9e0633ab002b6867da6c5b9b3277
Roushan Goswami <roushangoswami1378@gmail.com>
2025-06-18 15:54:54 +0530

    Add files via upload

---
+++ C/ifelse.c
@@ -0,0 +1,20 @@
+//WAP to Find max. from given 2 no. using if else statement 
+
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+ {
+ 	int a=-1213,b=-685; // a and b are variables
+ 	if(a>b)
+ 	{
+ 	printf("a is max!");	
+	 }
+ 	else
+ 	{
+ 		printf("b is max !");
+	 }
+ 	
+  } 
+ 
